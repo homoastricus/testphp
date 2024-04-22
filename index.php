edd267dd77692d2e7717d66d2f96733fd95c2f6b
astricus <homoastricus@koreana.spb.ru>
2024-04-22 20:41:30 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+memory_get_usage();
+
+function pr($data_name, $data): void
+{
+    echo $data_name . var_dump($data);
+    echo PHP_EOL . PHP_EOL;
+}
+
+$request_data = $_REQUEST ?? null;
+
+$get_data = isset($_GET) ?? null;
+
+$post_data = $_POST ?? null;
+
+pr("ENV", $_ENV);
+pr("POST", $post_data);
+pr("GET", $get_data);
+pr("REQUEST", $request_data);
+
+pr("COOKIE", $_COOKIE);
+pr("SESSION", $_SESSION);
+pr("SERVER", $_SESSION);
+pr("FILES", $_FILES);
+
+
+memory_get_usage();
+
+
